69c8604501d4dc76c26e47cb5842e8d65a8476eb
dcontrer83 <dcontrer83@yahoo.com>
2022-09-09 14:47:51 -0700

    Add onDelete

--- models/index.js
+++ models/index.js
@@ -4,26 +4,32 @@ const Comment = require('./Comment');
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
+    onDelete: 'cascade'
 });
 
 Blog.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: 'cascade'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
+    onDelete: 'cascade'
 });
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: 'cascade'
 });
 
 Comment.belongsTo(Blog, {
     foreignKey: 'blog_id',
+    onDelete: 'cascade'
 });
 
 Blog.hasMany(Comment, {
     foreignKey: 'blog_id',
+    onDelete: 'cascade'
 });
 
 
